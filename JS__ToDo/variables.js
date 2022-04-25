f0741ff744dd4279ef8af3f189b7f9c8b2e70702
Halushkov Dmytro <Halushkovdmitry93@gmail.com>
2022-04-25 10:46:29 +0300

    Update variables.js

--- JS__ToDo/variables.js
+++ JS__ToDo/variables.js
@@ -7,4 +7,9 @@ export const currentDate = new Date();
 export const regex = new RegExp("^[a-zA-Z0-9 ]+$");
 export const dateRegex = new RegExp("^([0-2][0-9]|(3)[0-1])(\-)(((0)[0-9])|((1)[0-2]))(\-)\\d{4}$");
 export const ENTER_KEY_CODE = 13;
-
+export const LIGHTGREY_COLOR = 'lightgrey';
+export const LINE_THROUGH = 'line-through';
+export const BLACK_COLOR = 'black';
+export const CHECKBOX = 'checkbox';
+export const NONE = 'none';
+export const BLOCK = 'block';
